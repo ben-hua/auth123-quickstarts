cfd04a2c7fdb4df5cd1863555fa52a5d3be419bd
Ben Hua <117179803+ben-hua@users.noreply.github.com>
2024-02-05 19:46:11 +0800

    support cors

--- example/quarkus-demo/src/main/java/org/acme/security/keycloak/authorization/PublicResource.java
+++ example/quarkus-demo/src/main/java/org/acme/security/keycloak/authorization/PublicResource.java
@@ -1,13 +1,27 @@
 package org.acme.security.keycloak.authorization;
 
+import java.time.LocalDate;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/public")
 public class PublicResource {
 
     @GET
-    public void serve() {
-        // no-op
+    public String serve() {
+        return "Hello from a public resource!";
     }
+
+    @GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Merchant Json() {
+        return new Merchant("hellow world to json");
+    }
+
 }
+
+record Merchant(String name){}
