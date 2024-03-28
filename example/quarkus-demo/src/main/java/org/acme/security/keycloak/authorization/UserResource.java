490c944030cf7f457a6819f05a9a94614e21835f
Ben Hua <117179803+ben-hua@users.noreply.github.com>
2024-03-28 16:45:42 +0800

    feat: add  a api to get user permissions

--- example/quarkus-demo/src/main/java/org/acme/security/keycloak/authorization/UserResource.java
+++ example/quarkus-demo/src/main/java/org/acme/security/keycloak/authorization/UserResource.java
@@ -1,20 +1,49 @@
 package org.acme.security.keycloak.authorization;
 
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.representations.idm.authorization.Permission;
+
+import io.quarkus.oidc.AccessTokenCredential;
 import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
 @Path("/api/users")
 public class UserResource {
 
     @Inject
-    SecurityIdentity keycloakSecurityContext;
+    SecurityIdentity identity;
+
+    @Inject
+    AuthzClient authzClient;
 
     @GET
     @Path("/me")
     public User me() {
-        return new User(keycloakSecurityContext);
+        return new User(identity);
+    }
+
+    @Produces("application/json")
+    @Path("/permissions")
+    @GET
+    public List<String> getPermissions() {
+        String accessToken = identity.getCredential(AccessTokenCredential.class).getToken();
+        String rpt = authzClient.authorization(accessToken).authorize().getToken();
+        List<Permission> permissions = authzClient.protection().introspectRequestingPartyToken(rpt).getPermissions();
+
+        return permissions.stream().mapMulti((Permission e, Consumer<String> c) -> {
+            c.accept(e.getResourceName());
+
+            if (e.getScopes() != null && !e.getScopes().isEmpty())
+                e.getScopes().forEach(scope -> c.accept(e.getResourceName() + ":" + scope));
+
+        }).collect(Collectors.toList());
     }
 
     public static class User {
